5567c0de52fd95354478c65ad7086ded4eb2b145
lain <lain@soykaf.club>
2019-10-11 16:13:13 +0200

    Migrations: Add clippy warning before long migration.

--- priv/repo/migrations/20191007160755_fill_object_ap_id_field.exs
+++ priv/repo/migrations/20191007160755_fill_object_ap_id_field.exs
@@ -1,7 +1,10 @@
 defmodule Pleroma.Repo.Migrations.FillObjectApIdField do
   use Ecto.Migration
+  alias Pleroma.Clippy
 
   def change do
+    Clippy.puts("ATTENTION! This migration might take hours! If you don't want to run it now, abort this with CTRL+C! I'll wait 30 seconds now.")
+    :times.sleep(:timer.seconds(30))
     execute("update objects set ap_id = data->>'id'")
     create unique_index(:objects, [:ap_id])
   end
