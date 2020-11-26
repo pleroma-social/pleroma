420efb844f5e0f629bbbecd4720e0af949671591
Haelwenn (lanodan) Monnier <contact@hacktivis.me>
2020-11-26 23:07:01 +0100

    common_fixes: Set sensitive when #nsfw is present

--- lib/pleroma/web/activity_pub/object_validators/common_fixes.ex
+++ lib/pleroma/web/activity_pub/object_validators/common_fixes.ex
@@ -34,6 +34,7 @@ defmodule Pleroma.Web.ActivityPub.ObjectValidators.CommonFixes do
     |> cast_and_filter_recipients("bto", follower_collection)
     |> cast_and_filter_recipients("bcc", follower_collection)
     |> Transmogrifier.fix_implicit_addressing(follower_collection)
+    |> Transmogrifier.set_sensitive()
   end
 
   def fix_activity_addressing(activity, _meta) do
